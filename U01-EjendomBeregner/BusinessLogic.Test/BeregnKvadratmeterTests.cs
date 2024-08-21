2b694c06c524562c2b5ed867640858e5ab5191ad
Kaj Bromose <kbr@ucl.dk>
2024-08-21 14:21:22 +0200

    Opgave2 Before

--- U01-EjendomBeregner/BusinessLogic.Test/BeregnKvadratmeterTests.cs
+++ U01-EjendomBeregner/BusinessLogic.Test/BeregnKvadratmeterTests.cs
@@ -1,9 +1,24 @@
-﻿namespace BusinessLogic.Test;
+﻿using EjendomBeregner.BusinessLogic;
+
+namespace BusinessLogic.Test;
 
 public class BeregnKvadratmeterTests
 {
     [Fact]
     public void Kvadratmeter_Sum_Stemmer_Med_Lejemaalsum()
     {
+        // Arrange
+        var lejemaals = new List<Lejemaal>
+        {
+            new() { Kvadratmeter = 5 },
+            new() { Kvadratmeter = 20 },
+            new() { Kvadratmeter = 30 }
+        };
+        var expected = lejemaals.Sum(l => l.Kvadratmeter);
+        var sut = new EjendomBeregnerService(...);
+        // Act 
+        var actual = sut.BeregnKvadratmeter();
+        // Assert
+        Assert.Equal(expected, actual);
     }
 }
---
+++ U01-EjendomBeregner/BusinessLogic.Test/LejemaalFileRepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using EjendomBeregner.BusinessLogic;
+
+namespace BusinessLogic.Test;
+
+public class LejemaalFileRepositoryTests
+{
+    [Theory]
+    [InlineData("1, 5, 2", 1, 5, 2)]
+    [InlineData("2, 10.8, 3", 2, 10.8, 3)]
+    [InlineData("3, 20.5, 4.5", 3, 20.5, 4.5)]
+    public void Given_Csv_Lines_Valid__Then_Lejemaal_Is_Returned(string csvLine, int expectedLejlighednummer,
+        double expectedKvadratmeter, double expectedRum)
+    {
+        // Arrange
+        var sut = new LejemaalFileRepository(...);
+        // Act
+        var actual = sut.HentLejemaal().First();
+        // Assert
+        Assert.Equal(expectedLejlighednummer, actual.Lejlighednummer);
+        Assert.Equal(expectedKvadratmeter, actual.Kvadratmeter, 5);
+        Assert.Equal(expectedRum, actual.Rum, 5);
+    }
+}
