39ba8ea621150368217442e6ecbe18940d07f1e5
Kaj Bromose <kbr@ucl.dk>
2024-08-22 08:35:00 +0200

    Xunit og Moq demo added

---
+++ Demokode/XunitOgMoqDemokode/Demo.XunitOgMoq.BusinessLogic/FooBar.cs
@@ -0,0 +1,34 @@
+﻿namespace Demo.XunitOgMoq.BusinessLogic;
+
+public interface IFoo
+{
+    string GetFoo();
+}
+
+public interface IBar
+{
+    string GetBar();
+}
+
+public class Foo : IFoo
+{
+    public string GetFoo()
+    {
+        return "Foo";
+    }
+}
+
+public class Bar : IBar
+{
+    private readonly IFoo _foo;
+
+    public Bar(IFoo foo)
+    {
+        _foo = foo;
+    }
+
+    public string GetBar()
+    {
+        return _foo.GetFoo() + "Bar";
+    }
+}
---
+++ Demokode/XunitOgMoqDemokode/Demo.XunitOgMoq.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Demo.XunitOgMoq.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
