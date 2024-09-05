c1099063c32ccd1f4fbfe09c154a85bd5b52b74c
Kaj Bromose <kbr@ucl.dk>
2024-09-05 16:22:27 +0200

    UnitOfWork added

---
+++ Demokode/OnionDemo/OnionDemo.Application/Helpers/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace OnionDemo.Application.Helpers;
+
+public interface IUnitOfWork
+{
+    void Commit();
+    void Rollback();
+    void BeginTransaction(IsolationLevel isolationLevel = IsolationLevel.Serializable);
+}
---
+++ Demokode/OnionDemo/OnionDemo.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using OnionDemo.Application.Helpers;
+
+namespace OnionDemo.Infrastructure;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DbContext _db;
+    private IDbContextTransaction? _transaction;
+
+    public UnitOfWork(DbContext db)
+    {
+        _db = db;
+    }
+
+    void IUnitOfWork.BeginTransaction(IsolationLevel isolationLevel)
+    {
+        if (_db.Database.CurrentTransaction != null) return;
+        _transaction = _db.Database.BeginTransaction(isolationLevel);
+    }
+
+    void IUnitOfWork.Commit()
+    {
+        if (_transaction == null) throw new Exception("You must call 'BeginTransaction' before Commit is called");
+        _transaction.Commit();
+        _transaction.Dispose();
+    }
+
+    void IUnitOfWork.Rollback()
+    {
+        if (_transaction == null) throw new Exception("You must call 'BeginTransaction' before Rollback is called");
+        _transaction.Rollback();
+        _transaction.Dispose();
+    }
+}
